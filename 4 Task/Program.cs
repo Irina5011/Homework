ff8d31797ba6fcbf04978306474363cba96382a9
Irina <89231707647@mail.ru>
2022-03-03 16:25:59 +0700

    program added

---
+++ 4 Task/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите число =>");  // вывод пргилащшеня для ввода
+string strValue = Console.ReadLine();   // ввод строки
+
+int value = int.Parse(strValue);        // преобразуем строку в целое число
+
+int A = 1;
+Console.Write(A+"");
+while (A<value)
+{
+    A = A+1;
+    Console.Write(A+"");
+}
